d0ace5d0a3f71804f07ec0179489950359aa7b49
Putbotz <96361236+Putbotz@users.noreply.github.com>
2023-12-23 01:14:00 +0800

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,190 @@
+import requests
+import os
+import subprocess
+import random
+import re
+import threading
+import urllib.request
+import argparse
+import sys
+from time import time
+
+output_file = 'proxy.txt'
+os.system('cls' if os.name == 'nt' else 'clear')
+
+if os.path.isfile(output_file):
+    os.remove(output_file)
+    print(f"File 'proxy.txt' telah dihapus.")
+
+print(f"Otw Download\n")
+
+proxy_urls = [
+'https://api.proxyscrape.com/v2/?request=displayproxies',
+'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/http/http.txt',
+'https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt',
+'https://raw.githubusercontent.com/yuceltoluyag/GoodProxy/main/raw.txt',
+'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt',
+'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt',
+'https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt',
+'https://proxyspace.pro/http.txt',
+'https://api.proxyscrape.com/?request=displayproxies&proxytype=http',
+'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt',
+'http://worm.rip/http.txt',
+'http://worm.rip/https.txt',
+'http://alexa.lr2b.com/proxylist.txt',
+'https://api.openproxylist.xyz/http.txt',
+'http://rootjazz.com/proxies/proxies.txt',
+'https://multiproxy.org/txt_all/proxy.txt',
+'https://proxy-spider.com/api/proxies.example.txt',
+'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt',
+'https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies.txt',
+'https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt',
+'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt',
+'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt',
+'https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt',
+'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all',
+'https://www.proxydocker.com/en/proxylist/download?email=noshare&country=all&city=all&port=all&type=all&anonymity=all&state=all&need=all',
+'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=anonymous',
+"https://www.proxy-list.download/api/v1/get?type=http",
+"https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+'https://github.com/Tsprnay/Proxy-lists/raw/master/proxies/http.txt',
+'https://github.com/Tsprnay/Proxy-lists/raw/master/proxies/https.txt',
+'https://github.com/proxy4parsing/proxy-list/raw/main/http.txt',
+'https://github.com/vakhov/fresh-proxy-list/raw/master/http.txt',
+'https://github.com/vakhov/fresh-proxy-list/raw/master/https.txt',
+'https://github.com/TuanMinPay/live-proxy/raw/master/http.txt',
+'https://github.com/yemixzy/proxy-list/raw/main/proxies/unchecked.txt',
+'https://github.com/andigwandi/free-proxy/raw/main/proxy_list.txt',
+'https://github.com/elliottophellia/yakumo/raw/master/results/http/global/http_checked.txt',
+'https://raw.githubusercontent.com/vakhov/fresh-proxy-list/master/http.txt',
+'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/http/http.txt',
+'https://raw.githubusercontent.com/shiftytr/proxy-list/master/proxy.txt',
+'https://api.openproxylist.xyz/http.txt',
+'https://proxy-spider.com/api/proxies.example.txt',
+'https://naawy.com/api/public/proxylist/getList/?proxyType=http&format=txt',
+'https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/http.txt',
+'https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt',
+]
+
+def download_and_save_proxies(url, output_file):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(output_file, 'a') as file:
+                file.write(response.text)
+                print(f"Collect {url}")
+        else:
+            print(f"Gagal {url}")
+    except Exception as e:
+        print(f"Gagal {url}")
+
+open(output_file, 'w').close()
+
+class Proxy:
+    def __init__(self, method, proxy):
+        if method.lower() not in ["http", "https"]:
+            raise NotImplementedError("Only HTTP and HTTPS are supported")
+        self.method = method.lower()
+        self.proxy = proxy
+
+    def is_valid(self):
+        return re.match(r"\d{1,3}(?:\.\d{1,3}){3}(?::\d{1,5})?$", self.proxy)
+
+    def check(self, site, timeout, user_agent):
+        url = self.method + "://" + self.proxy
+        proxy_support = urllib.request.ProxyHandler({self.method: url})
+        opener = urllib.request.build_opener(proxy_support)
+        urllib.request.install_opener(opener)
+        req = urllib.request.Request(self.method + "://" + site)
+        req.add_header("User-Agent", user_agent)
+        try:
+            start_time = time()
+            urllib.request.urlopen(req, timeout=timeout)
+            end_time = time()
+            time_taken = end_time - start_time
+            return True, time_taken, None
+        except Exception as e:
+            return False, 0, e
+
+    def __str__(self):
+        return self.proxy
+
+def verbose_print(verbose, message):
+    if verbose:
+        print(message)
+
+def check(file, timeout, method, site, verbose, random_user_agent):
+    proxies = []
+    with open(file, "r") as f:
+        for line in f:
+            proxies.append(Proxy(method, line.replace("\n", "")))
+
+    print(f"Checking {len(proxies)} Proxy")
+    proxies = filter(lambda x: x.is_valid(), proxies)
+    valid_proxies = []
+    user_agent = random.choice(user_agents)
+
+    def check_proxy(proxy, user_agent):
+        new_user_agent = user_agent
+        if random_user_agent:
+            new_user_agent = random.choice(user_agents)
+        valid, time_taken, error = proxy.check(site, timeout, new_user_agent)
+        message = {
+            True: f"{proxy} is valid, took {time_taken} seconds",
+            False: f"{proxy} is invalid: {repr(error)}",
+        }[valid]
+        verbose_print(verbose, message)
+        valid_proxies.extend([proxy] if valid else [])
+
+    threads = []
+    for proxy in proxies:
+        t = threading.Thread(target=check_proxy, args=(proxy, user_agent))
+        threads.append(t)
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    with open(file, "w") as f:
+        for proxy in valid_proxies:
+            f.write(str(proxy) + "\n")
+
+    print(f"Found {len(valid_proxies)} valid proxies")
+
+
+def verbose_print(verbose, message):
+    if verbose:
+        print(message)
+
+for url in proxy_urls:
+    download_and_save_proxies(url, output_file)
+    
+with open('proxy.txt', 'r') as ceki:
+    jumlh = sum(1 for line in ceki)
+    
+print(f"\n{jumlh} Proxy Sudah Di Unduh, Mau Check Y/N: ", end="")
+choice = input().strip().lower()
+
+if choice == 'y' or choice == 'Y':
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15",
+    ]
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--timeout", type=int, default=20, help="Dismiss the proxy after -t seconds")
+    parser.add_argument("-p", "--proxy", default="http", help="Check HTTPS or HTTP proxies")
+    parser.add_argument("-s", "--site", default="https://google.com/", help="Check with specific website like google.com")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
+    parser.add_argument("-r", "--random_agent", action="store_true", help="Use a random user agent per proxy")
+    
+    args = parser.parse_args()
+    check(file=output_file, timeout=args.timeout, method=args.proxy, site=args.site, verbose=args.verbose, random_user_agent=args.random_agent)
+    sys.exit(0)
+else:
+    print(f"Terima Kasih, Telah Menggunakan Script Saya!.\n")
